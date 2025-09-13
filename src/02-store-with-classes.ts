92fd03708ca3367b75317273a51113ad6bdb92bb
Stephan Kulla <git.mail@kulla.me>
2025-09-13 11:03:50 +0200

    feat(store-with-classes): Add simple example

--- src/02-store-with-classes.ts
+++ src/02-store-with-classes.ts
@@ -11,8 +11,43 @@ class EditorStore {
     return key
   }
 
+  getEntries() {
+    return Array.from(this.entities.entries())
+  }
+
   private generateKey() {
     this.lastKey += 1
     return this.lastKey.toString() as Key
   }
+}
+
+abstract class EditorNode<JsonValue = unknown> {
+  constructor(
+    protected store: EditorStore,
+    protected jsonValue: JsonValue,
+  ) {}
+
+  abstract storeValue(): Key
+}
+
+class TextNode extends EditorNode<string> {
+  storeValue() {
+    return this.store.insert(() => this.jsonValue)
+  }
+}
+
+class TextContent extends EditorNode<string[]> {
+  storeValue() {
+    return this.store.insert(() =>
+      this.jsonValue.map((text) => new TextNode(this.store, text).storeValue()),
+    )
+  }
+}
+
+const store = new EditorStore()
+
+new TextContent(store, ['Hello', ' ', 'World!']).storeValue()
+
+for (const [key, value] of store.getEntries()) {
+  console.log(`${key}: ${value}`)
 }
