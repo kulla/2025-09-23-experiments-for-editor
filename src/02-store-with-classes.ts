3c1d299f5971044d6746d38f7f3397cd09ef3735
Stephan Kulla <git.mail@kulla.me>
2025-09-13 10:57:12 +0200

    chore: Update biome config

--- src/02-store-with-classes.ts
+++ src/02-store-with-classes.ts
@@ -1,16 +1,18 @@
+type Key = string & { __brand: 'Key' }
+
 class EditorStore {
-	private lastKey = 0;
-	private entities = new Map<string, unknown>();
+  private lastKey = 0
+  private entities = new Map<Key, unknown>()
 
-	insert(createValue: (key: string) => unknown) {
-		const key = this.generateKey();
-		const value = createValue(key);
-		this.entities.set(key, value);
-		return key;
-	}
+  insert(createValue: (key: Key) => unknown) {
+    const key = this.generateKey()
+    const value = createValue(key)
+    this.entities.set(key, value)
+    return key
+  }
 
-	private generateKey() {
-		this.lastKey += 1;
-		return this.lastKey.toString();
-	}
+  private generateKey() {
+    this.lastKey += 1
+    return this.lastKey.toString() as Key
+  }
 }
