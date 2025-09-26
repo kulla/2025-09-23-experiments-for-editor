2c241592c661c9f6394dc921cc6b1f95f162f3e2
Stephan Kulla <git.mail@kulla.me>
2025-09-26 16:23:51 +0200

    feat(prototype-builder): Add class AbstractType

--- src/10-prototype-builder.ts
+++ src/10-prototype-builder.ts
@@ -6,6 +6,7 @@ interface Foo {
 }
 
 type PrototypeOf<T extends object> = O.Select<T, F.Function> & ThisType<T>
+type AbstractPrototypeOf<T extends object> = Partial<PrototypeOf<T>>
 type DataOf<T extends object> = O.Filter<T, F.Function>
 
 export type FooPrototype = PrototypeOf<Foo> // { getBar: () => string }
@@ -17,9 +18,31 @@ export const fooProtoTest: FooPrototype = {
   },
 }
 
-interface AbstractType<T extends object, A extends Partial<PrototypeOf<T>>> {
-  readonly __type__: T
-  readonly prototype: A
+class AbstractType<T extends object, P extends AbstractPrototypeOf<T>> {
+  constructor(public readonly prototype: P) {}
+
+  add<P2 extends AbstractPrototypeOf<T>>(prototype: P2) {
+    return new AbstractType<T, P & P2>({ ...this.prototype, ...prototype })
+  }
+
+  extend<P2 extends AbstractPrototypeOf<T>>(
+    ext: (Base: P) => P2,
+  ): AbstractType<T, P & P2> {
+    return this.add(ext(this.prototype))
+  }
+
+  extendType<T2 extends object>() {
+    return {
+      updateImplForNewType: <P2 extends AbstractPrototypeOf<T2>>(
+        ext: (Base: AbstractPrototypeOf<T2>) => P2,
+      ): AbstractType<T2, P2> =>
+        new AbstractType<T2, P2>(ext({} as AbstractPrototypeOf<T2>)),
+    }
+  }
+
+  static startImplementation<T extends object>() {
+    return new AbstractType<T, object>({})
+  }
 }
 
 interface ConcreteType<T extends object>
