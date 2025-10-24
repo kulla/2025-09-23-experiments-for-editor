1ae80a4a9c9203e4724641a1df75f0c99b2bc793
Stephan Kulla <git.mail@kulla.me>
2025-10-24 11:08:20 +0200

    Add/update src/28-editor-api.js

---
+++ src/28-editor-api.js
@@ -0,0 +1,35 @@
+export function MultipleChoiceExercise(context) {
+  const { register } = context
+
+  const MultipleChoiceAnswers = register
+    .createNodeType('MultipleChoiceAnswers')
+    .extend(
+      array(
+        register
+          .createNodeType('MultipleChoiceAnswer')
+          .extend(
+            object(
+              parameter('isCorrect', BooleanType(context)),
+              parameter('answer', TextType(context)),
+            ),
+          )
+          .save(),
+      ),
+    )
+    .save()
+
+  return register
+    .createNodeType('MultipleChoiceExercise')
+    .extend(
+      object(
+        parameter('exercise', Paragraph(context)),
+        parameter('answers', MultipleChoiceAnswers),
+      ),
+    )
+    .extend({
+      render() {
+        return null
+      },
+    })
+    .save()
+}
