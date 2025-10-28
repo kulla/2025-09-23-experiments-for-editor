a9c825aaa48ffa43de207154145c0f5a2f9628ad
Stephan Kulla <git.mail@kulla.me>
2025-10-28 11:47:41 +0100

    Add/update src/35-schema-example.ts

--- src/35-schema-example.ts
+++ src/35-schema-example.ts
@@ -34,7 +34,7 @@ type _JSONValue<S> = S extends StringSchema
 
 type JSONValue<S> = [S] extends [unknown] ? _JSONValue<S> : never
 
-interface NestedNode<S extends Schema> {
+interface NestedNode<S extends Schema = Schema> {
   schema: S
   value: JSONValue<S>
 }
@@ -42,7 +42,7 @@ interface NestedNode<S extends Schema> {
 type Branded<T, B> = T & { __brand: B }
 type Key = Branded<string, 'Key'>
 
-type FlatValue<S extends Schema> = S extends ObjectSchema<infer T>
+type FlatValue<S = Schema> = S extends ObjectSchema<infer T>
   ? { [K in keyof T]: Key }
   : S extends ArraySchema
     ? ReadonlyArray<Key>
@@ -52,6 +52,38 @@ interface FlatNode<S extends Schema = Schema> {
   schema: S
   value: FlatValue<S>
 }
+
+interface URItoKind<A extends Schema> {
+  FlatNode: FlatNode<A>
+  NestedNode: NestedNode<A>
+}
+type URIS = keyof URItoKind<Schema>
+type Kind<F extends URIS, A extends Schema> = URItoKind<A>[F]
+
+interface NodeClass<F extends URIS> {
+  getProperty<T extends Record<string, Schema>, K extends keyof T>(
+    node: Kind<F, ObjectSchema<T>>,
+    key: K,
+  ): Kind<F, T[K]>
+}
+
+const NestedNodeClass: NodeClass<'NestedNode'> = {
+  getProperty(node, key) {
+    const value = node.value[key]
+    return { schema: node.schema.properties[key], value }
+  },
+}
+
+const FlatNodeClass = (map: Map<Key, FlatValue>): NodeClass<'FlatNode'> => ({
+  getProperty(node, key) {
+    const keyValue = node.value[key]
+    const value = map.get(keyValue) as FlatValue<
+      (typeof node.schema.properties)[typeof key]
+    >
+
+    return { schema: node.schema.properties[key], value }
+  },
+})
 
 export function jsonGetProperty<
   T extends Record<string, Schema>,
@@ -68,12 +100,10 @@ export function flatGetProperty<
 >(
   p: FlatNode<ObjectSchema<P>>,
   key: K,
-  map: Map<Key, FlatNode>,
+  map: Map<Key, FlatValue>,
 ): FlatNode<P[K]> {
-  const value = p.value[key] as Key
-  const childNode = map.get(value)
-  if (!childNode) {
-    throw new Error('Key not found in map')
-  }
-  return childNode as FlatNode<P[K]>
+  const keyValue = p.value[key]
+  const value = map.get(keyValue) as FlatValue<P[K]>
+
+  return { schema: p.schema.properties[key], value }
 }
