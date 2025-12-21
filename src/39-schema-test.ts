a7a4c2541704729605dab4dcc1324c9592e979f8
Stephan Kulla <git.mail@kulla.me>
2025-12-22 00:08:52 +0100

    feat: Add schema test with higher order functions

--- src/39-schema-test.ts
+++ src/39-schema-test.ts
@@ -1,4 +1,6 @@
-type Factory<I, O> = (arg: I) => O
+type Factory<I extends Schema[], O> = (...args: I) => O
+
+type Output<F> = F extends Factory<infer _I, infer O> ? O : never
 
 interface Schema<F = unknown> {
   isJsonValue(value: unknown): value is F
@@ -6,37 +8,59 @@ interface Schema<F = unknown> {
 
 type JSONValue<S extends Schema> = S extends Schema<infer U> ? U : never
 
-const booleanSchema = ((): Schema<boolean> => ({
-  isJsonValue(value: unknown) {
-    return typeof value === 'boolean'
-  },
-})) satisfies Factory<never, Schema<boolean>>
+const booleanSchema = withGuard(
+  (): Schema<boolean> => ({
+    isJsonValue(value: unknown) {
+      return typeof value === 'boolean'
+    },
+  }),
+)
 
-const numberSchema = ((): Schema<number> => ({
-  isJsonValue(value: unknown) {
-    return typeof value === 'number'
-  },
-})) satisfies Factory<never, Schema<number>>
+const numberSchema = withGuard(
+  (): Schema<number> => ({
+    isJsonValue(value: unknown) {
+      return typeof value === 'number'
+    },
+  }),
+)
 
-const stringSchema = ((): Schema<string> => ({
-  isJsonValue(value: unknown) {
-    return typeof value === 'string'
-  },
-})) satisfies Factory<never, Schema<string>>
+const stringSchema = withGuard(
+  (): Schema<string> => ({
+    isJsonValue(value: unknown) {
+      return typeof value === 'string'
+    },
+  }),
+)
 
-const unionSchema = <F extends Schema[]>(
-  ...schemas: F
-): Schema<JSONValue<F[number]>> => ({
-  isJsonValue(value: unknown): value is JSONValue<F[number]> {
-    return schemas.some((schema) => schema.isJsonValue(value))
-  },
-})
+const unionSchema = withGuard(
+  <F extends Schema[]>(...schemas: F): Schema<JSONValue<F[number]>> => ({
+    isJsonValue(value: unknown): value is JSONValue<F[number]> {
+      return schemas.some((schema) => schema.isJsonValue(value))
+    },
+  }),
+)
 
-export const BooleanSchema = booleanSchema()
-export const NumberSchema = numberSchema()
-export const StringSchema = stringSchema()
-export const BooleanOrNumberSchema = unionSchema(
+export const BooleanSchema = booleanSchema.create()
+export const NumberSchema = numberSchema.create()
+export const StringSchema = stringSchema.create()
+export const BooleanOrNumberSchema = unionSchema.create(
   BooleanSchema,
   NumberSchema,
   StringSchema,
 )
+
+function withGuard<F extends Factory<Schema[], object>>(
+  factory: F,
+): { create: F; is: (value: unknown) => value is Output<F> } {
+  const typeSymbol = Symbol()
+
+  return {
+    create: ((arg) => ({
+      [typeSymbol]: true,
+      ...factory(arg),
+    })) as F,
+    is(value: unknown): value is Output<F> {
+      return typeof value === 'object' && value !== null && typeSymbol in value
+    },
+  }
+}
