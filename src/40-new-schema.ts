bcb847f376bbb1574d8e06531dcb649109f107d9
Stephan Kulla <git.mail@kulla.me>
2026-06-08 19:31:07 +0200

    feat: Add src/40-new-schema.ts

---
+++ src/40-new-schema.ts
@@ -0,0 +1,21 @@
+interface Methods {
+  toString(): string
+}
+
+interface Text {
+  text: string
+}
+
+function textToString(this: Text) {
+  return this.text
+}
+
+class A implements Text {
+  constructor(public text: string) {}
+
+  toString = textToString
+}
+
+const foo = new A("Hello World")
+
+console.log(foo.toString())
